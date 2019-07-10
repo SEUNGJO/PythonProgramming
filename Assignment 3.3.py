b56b527df38bfab343b01c2bc2209831964d7dc1
SEUNGJO <49353138+SEUNGJO@users.noreply.github.com>
2019-07-10 12:20:40 -0700

    Update and rename Assignment 3.3 to Assignment 3.3.py

--- Assignment 3.3.py
+++ Assignment 3.3.py
@@ -1,3 +1,4 @@
+'''
 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. 
 If the score is between 0.0 and 1.0, print a grade using the following table:
 Score Grade
@@ -11,6 +12,7 @@ If the user enters a value out of range, print a suitable error message and exit
 
 Desired Output
 B
+'''
 
 s=input('Enter Score:')
 try:
